2e887dceb6606c6d7dfbc71558dbd4d87c5d2a83
Syd <sydwelldavin@gmail.com>
2024-06-20 13:37:20 -0500

    Adjusting CSS

--- src/components/OptionsProfitCalculator.jsx
+++ src/components/OptionsProfitCalculator.jsx
@@ -1,9 +1,23 @@
 import { useState, useEffect } from 'react';
+import './options.css';
 
 function OptionsProfitCalculator(data) {
 
+    useEffect(() => {
+        console.log(data);
+    }, [data])
+
     return (
-        <><h1>Options Profit Calculator</h1></>
+        <div className='wrap-options-calc'>
+            <div className='wrap-options-flex-col'>
+                <div className='wrap-options-flex-row center-row'>
+                    <h1>Options Profit Calculator</h1>
+                </div>
+                <div className='wrap-options-flex-row'>
+                    <div>Graph</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
