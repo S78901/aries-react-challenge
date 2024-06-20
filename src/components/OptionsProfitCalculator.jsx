bf53b3747ab4f30cedbd32c6e4f92dd16e01c64c
Syd <sydwelldavin@gmail.com>
2024-06-20 13:08:30 -0500

    Initial scaffolding

---
+++ src/components/OptionsProfitCalculator.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+
+function OptionsProfitCalculator(data) {
+
+    return (
+        <><h1>Options Profit Calculator</h1></>
+    );
+}
+
+export default OptionsProfitCalculator;
